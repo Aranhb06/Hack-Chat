d9423234ac7a7522458e03dcce34e0ac159c0088
Aranhb06 <aranhb06@gmail.com>
2026-02-07 18:05:47 +0100

    Añadiendo arte ascci para el docker logs

--- start.sh
+++ start.sh
@@ -8,5 +8,39 @@ fi
 
 # Lanzamiento de los servicios
 /usr/sbin/sshd     # Lanza SSH en segundo plano
-sleep 2
-exec tor -f /etc/tor/torrc
+tor -f /etc/tor/torrc &
+
+# Mensaje de espera
+echo "Esperando a que la red Tor se estabilice..."
+sleep 10
+
+# Mensaje de bienvenida
+echo '
+                      :::!~!!!!!:.
+                  .xUHWH!! !!?M88WHX:.
+                .X*#M@$!!  !X!M$$$$$$WWx:.
+               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+             :X- M$$$$       `"T#$T~!8$WUXU~
+            :%`  ~#$$$m:        ~!~ ?$$$$$$
+          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+.....   -~~:<` !    ~?T#$$@@W@*?$$      /`
+W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+$R@i.~~ !     :   ~$$$$$B$$en:``
+?MXT@Wx.~    :     ~"##*$$$$M~
+'
+
+echo ""
+echo "      SERVIDOR LEVANTADO        "
+echo ""
+echo "Link - $(cat /var/lib/tor/other_hidden_service/hostname)"
+# Mantener conexion activa
+tail -f /dev/null
